af9099dde29195f31f0d1d3daad49884cdae26f6
Ashish Trehan <ashishtrehan@Ashishs-MacBook-Pro.local>
2021-04-29 09:45:09 -0400

    adjusting fitness function

--- src/develop.py
+++ src/develop.py
@@ -14,17 +14,21 @@ def null(x):
 
 
 def nutrition(meals):
-    p,s,c = [],[],[]
+    p,s,c,f,sd = [],[],[],[],[]
     for x in meals:
         b = Values(food_dictionary.get(x))
         sugars = null(b.sugars)
         protein = null(b.protein)
         cal = null(b.calories)
+        fat = null(b.fat)
+        sodium = null(b.sodium)
         # print (b.group)
         p.append(protein)
         s.append(sugars)
         c.append(cal)
-    return np.array((p,c,s)).T
+        f.append(fat)
+        sd.append(sodium)
+    return np.array((p,c,s,f,sd)).T
         
 
 def get_fitness(x):
@@ -33,8 +37,17 @@ def get_fitness(x):
     sigma = np.sum(m,axis=0)
     # sugar_constraint = np.where(sigma[2]>0,1,0)
     # print (sugar_constraint)
-    return sigma[0]*4.0/sigma[1]
-    
+    fat = sigma[3]/sigma[1]
+    sodium = 1 if sigma[4] <= 2300 else 0
+    # pro = (sigma[0]*4.0)/sigma[1]
+    pro = f(sigma[0]*4.0,sigma[1])
+    a = 1 if pro >= .3 else 0
+    return a - fat - sodium
+    # return fat*9/sigma[1]
+
+def f(x, y):
+    "fixing division by null"
+    return (0 if x==0 and y==0 else float(x**2*y)/(x**4 + y**4))
 
 
 def genetic_algorithm(generations,population_size):
@@ -47,8 +60,8 @@ def genetic_algorithm(generations,population_size):
         for i in range(0,population_size,2):
             p1,p2 = _generate_selection(pop),_generate_selection(pop)
             c1,c2 = _generate_crossover(p1,p2,.5,get_fitness)
-            c1 = _mutate(c1,ids,get_fitness,.25)
-            c2 = _mutate(c2,ids,get_fitness,.18)
+            c1 = _mutate(c1,ids,get_fitness,.15)
+            c2 = _mutate(c2,ids,get_fitness,.20)
             children.append(c1)
             children.append(c2)
         for z in children:
@@ -61,8 +74,8 @@ def genetic_algorithm(generations,population_size):
         pop = children
         max_fitness = max([x.fitness for x in children])
         metrics.append([{'generation':x,'max_fitness':max_fitness}])
-    print (metrics)
+    # print (metrics)
     return c,children
 
 
-c,children = (genetic_algorithm(100,100))
+c,children = (genetic_algorithm(100,1000))
